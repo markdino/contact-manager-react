27adf6cb3a26057589eada51289a2c5fe1044690
Mark Dino <markdinopelonia447@gmail.com>
2020-07-05 11:50:41 +0800

    feat: LogForm component (Logn & Signup from)

---
+++ src/components/logForm.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import Axios from "axios";
+import { ReactComponent as Close } from '../assets/svg/times.svg'
+
+class LogForm extends Component {
+    state = {
+        username: '',
+        password: '',
+        response: null
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    reset = () => {
+        this.setState({
+            username: '',
+            password: '',
+            form: '',
+            response: null
+        })
+    }
+
+    close = () => {
+        this.reset()
+        this.props.onClose()
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        const { username, password, form } = this.state
+        Axios.post(`http://localhost:1234/api/user/${form}`, { username, password }, { withCredentials: 'include' })
+            .then(payload => {
+                console.log(payload)
+                this.props.setUser(payload.data.value)
+                this.close()
+            })
+            .catch(payload => {
+                if (!payload.response)
+                    return this.setState({
+                        response: {
+                            error: 'Server is down or connection to the server refused.',
+                            name: 'Connection Refused',
+                            value: null,
+                        }
+                    });
+
+                this.setState({ response: payload.response.data })
+            })
+    }
+
+    componentDidMount() {
+        this.setState({ form: this.props.form })
+    }
+
+    render() {
+        const { username, password, response, form } = this.state
+        const closeStyle = {
+            width: '10px',
+            fill: '#6c757d',
+            padding: '10px',
+            cursor: 'pointer',
+            margin: '-20px -10px 0px 0px'
+        }
+        return (
+            <section id="img-modal">
+                <section className="row align-item-center modal-wrapper">
+                    <main className="modal-container">
+                        <section className="d-flex row-reverse">
+                            <Close style={closeStyle} onClick={this.close} />
+                        </section>
+                        <header className="row flex-space-between">
+                            <h1
+                                className={form === 'signup' ? 'text-primary' : 'text-muted cursor-pointer'}
+                                onClick={() => this.setState({ form: 'signup' })}
+                            >
+                                Sign Up
+                            </h1>
+                            <h1 className="text-muted">|</h1>
+                            <h1
+                                className={form === 'login' ? 'text-success' : 'text-muted cursor-pointer'}
+                                onClick={() => this.setState({ form: 'login' })}
+                            >
+                                Login
+                            </h1>
+                        </header>
+                        <form onSubmit={this.onSubmit}>
+                            <section className="input-group">
+                                <label htmlFor="username">Username</label>
+                                <input
+                                    type="username"
+                                    name="username"
+                                    id="username"
+                                    value={username}
+                                    onChange={this.onChange}
+                                    required
+                                />
+                            </section>
+                            <section className="input-group">
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    value={password}
+                                    onChange={this.onChange}
+                                    required
+                                />
+                            </section>
+                            <section className='invalid-feedback'>
+                                {(response) ? response.error : ''}
+                            </section>
+                            <section className="buttons">
+                                <button
+                                    type="submit"
+                                    className={form === 'signup' ? 'btn-primary' : 'btn-success'}
+                                >
+                                    {form}
+                                </button>
+                            </section>
+                        </form>
+                    </main>
+                </section>
+            </section>
+        );
+    }
+}
+
+export default LogForm;
